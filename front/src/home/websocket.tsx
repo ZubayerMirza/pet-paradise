c999e5622298a26bc11671707bce8f6ed76eeb14
Zubayer Mirza <56887234+ZubayerMirza@users.noreply.github.com>
2024-05-08 10:59:13 -0400

    Delete front directory

--- front/src/home/websocket.tsx
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-export const PORT = "http://localhost:8000/"
-
-// working with same domain with the server
-const socket = io(PORT);
-
-export default socket;
