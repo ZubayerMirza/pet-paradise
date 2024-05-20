9f31517d5f73bf52055d1a1b9b43ed8779c334b7
ijustwanttoeat90 <rh8012@naver.com>
2024-05-20 09:19:11 -0400

    signup UI changed

--- front/src/users/Signup.tsx
+++ front/src/users/Signup.tsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
-import './login.css';
-import '../home/home.css';
+import './signup.css';
+import Logo from '../assets/logo.png';
 import { useNavigate } from 'react-router';
 
 function SignUp() {
@@ -14,10 +14,13 @@ function SignUp() {
       
       const testcase = { 
         username: e.target.username.value, 
-        password: e.target.password.value
+        password: e.target.password.value,
+        confirm: e.target.confirm.value,
+        email:e.target.email.value
       };
-
-      fetch('http://localhost:8000/signup', {
+      
+      if(testcase.confirm ===testcase.password){
+          fetch('http://localhost:8000/signup', {
       method: "POST",
       headers: { 
         "Content-Type": "application/json",
@@ -37,24 +40,44 @@ function SignUp() {
     .catch(error => {
       console.error('Something wrong', error);
     })
+    // console.log('same')  
+      }else(
+        alert('Password should be same')
+      )
+    
     
   }
 
     return(
       <> 
-      <div className='home'>
-      <form className='LoginForm' onSubmit={OnSubmitHandler}>   
-      <div className='LoginBox' onSubmit={OnSubmitHandler}>
-      <label><b>Username</b> 
-      <input type="text" name="username" placeholder="Username" required></input></label>
+      <div className='signBox'>
+      <form className='signForm' onSubmit={OnSubmitHandler}>   
+      <div className='signBoxL'>
+        <div className='backLogo'></div>
+      {/* <img src={Logo} style={{height:"80%", width: "80%" }} alt="logo"></img> */}
       </div>
-      <div className='LoginBox'>
-      <label><b>Password</b>
-      <input id="input" type="password" name="password" placeholder="Password" required></input></label>
+      <div className='signBoxR'>
+        <h1>Sign Up</h1>
+        <div className='typeBox' onSubmit={OnSubmitHandler}>
+      <p>Username</p>
+      <input type="text" name="username" placeholder="  Username" required></input>
       </div>
-      <div className='LoginBox'>
-      <button id="CrtBtn" type="submit">Create Account</button>
+      <div className='typeBox'>
+      <p> email</p>
+      <input  type="email" name="email" pattern="[A-Za-z0-9!@#$%^&*()+-]+@[a-z]+.[a-z]{3}"placeholder="  email@example.com" required></input>
       </div>
+      <div className='typeBox'>
+      <p>Password</p>
+      <input  type="password" name="password" placeholder=" Password" required></input>
+      </div>
+      <div className='typeBox'>
+      <p>Confirm Password</p>
+      <input  type="password" name="confirm" placeholder=" Confirm password" required></input>
+      </div>
+      <div className='typeBox'>
+      <button  type="submit">Create Account</button>
+      </div></div>
+      
       </form>
       </div>
       </>
